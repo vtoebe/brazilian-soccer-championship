ab9e30ca158158cd7e9659c51a4f433b1012a26c
maiconwps <maiconobmep@hotmail.com>
2022-02-07 18:11:56 -0400

    feat: add output ui icon.

--- src/main/java/com/letscode/brazilian_soccer_championship/ui/output/OutputUI.java
+++ src/main/java/com/letscode/brazilian_soccer_championship/ui/output/OutputUI.java
@@ -3,6 +3,7 @@ package com.letscode.brazilian_soccer_championship.ui.output;
 import com.letscode.brazilian_soccer_championship.entities.Game;
 import com.letscode.brazilian_soccer_championship.entities.Ranking;
 import com.letscode.brazilian_soccer_championship.entities.Team;
+import com.letscode.brazilian_soccer_championship.ui.shared.CustomIcon;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,6 +12,7 @@ import java.util.Set;
 public class OutputUI {
     public static void show(Set<Game> games, Set<Team> teams, Ranking ranking){
         var frame = new JFrame("Brasileirão");
+        frame.setIconImage(CustomIcon.buildLogoIcon(50, 50).getImage());
         var mainOutputUI = new MainOutputUI(games, teams, ranking);
         frame.setContentPane(mainOutputUI.getTabbedPane());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
