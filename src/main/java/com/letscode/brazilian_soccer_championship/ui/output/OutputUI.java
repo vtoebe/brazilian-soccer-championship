637c854261261a1e58ba686f15e30c8f7b98c8da
Vitória Toebe <vitoriatoebe@gmail.com>
2022-02-06 23:44:14 -0300

    Update OutputUI.java

--- src/main/java/com/letscode/brazilian_soccer_championship/ui/output/OutputUI.java
+++ src/main/java/com/letscode/brazilian_soccer_championship/ui/output/OutputUI.java
@@ -5,12 +5,17 @@ import com.letscode.brazilian_soccer_championship.entities.Ranking;
 import com.letscode.brazilian_soccer_championship.entities.Team;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.Set;
 
 public class OutputUI {
     public static void show(Set<Game> games, Set<Team> teams, Ranking ranking){
-        JFrame gamesByTeam = TeamUI.build(teams);
-        gamesByTeam.setVisible(true);
-
+        var frame = new JFrame("Brasileirão");
+        var mainOutputUI = new MainOutputUI(games, teams, ranking);
+        frame.setContentPane(mainOutputUI.getTabbedPane());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(1000, 1000);
+        frame.setVisible(true);
     }
 }
